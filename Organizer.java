cdaa230f478129b45fda02790b081fd56fc7a590
ChocoCodes <chocojhong999@gmail.com>
2024-04-08 14:42:31 +0800

    Added system core files

---
+++ Organizer.java
@@ -0,0 +1,5 @@
+public class Organizer extends FileOperations {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ TableOfficial.java
@@ -0,0 +1,5 @@
+public class TableOfficial extends FileOperations {
+    public static void main(String[] args) {
+        
+    }
+}
